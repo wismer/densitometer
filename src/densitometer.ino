42b9b1c40caf0d71d7ad577c807f698a497fc348
Matt Long <matthewhl@gmail.com>
2017-08-01 16:38:17 -0400

    init

---
+++ src/densitometer.ino
@@ -0,0 +1,18 @@
+// include necessary libs
+
+// I suppose this is a an idea for what the output would resemble. You'll have to fill me in on what on how the device will
+// read light
+struct RGBA {
+  byte red;
+  byte green;
+  byte blue;
+  byte density;
+};
+
+void setup() {
+  // device sensor initialization and DB initialization
+}
+
+void loop() {
+  // is this device going to be "always on", i.e. you turn the sensor on, and scan away, or are you using a button to activate the sensor
+}
